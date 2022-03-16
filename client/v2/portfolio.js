b3fb0e453fb378e2bd251df8674bc6f51acd9ea3
acharruey <adrien.charruey@gmail.com>
2022-03-17 00:52:49 +0100

    msg

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -116,6 +116,12 @@ selectShow.addEventListener('change', async (event) => {
   render(currentProducts, currentPagination);
 });
 
+selectPage.addEventListener('change', event => {
+  fetchProducts(parseInt(event.target.value), currentPagination.pageCount)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination));
+})
+
 document.addEventListener('DOMContentLoaded', async () => {
   const products = await fetchProducts();
 
